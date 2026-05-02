5ecc2c9b7d3591cb9736dd4881834f29b2b40946
ARUN KUMAR <148804375+Arun979321@users.noreply.github.com>
2026-05-03 04:33:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173", 
   "http://127.0.0.1:5173", 
-  "https://taskorbit-production.up.railway.app"
+  "https://taskorbit-production.up.railway.app",
   "https://taskorbit-production.up.railway.app/api"
  
 ];
